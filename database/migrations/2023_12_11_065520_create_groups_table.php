306340ebb1f139f66291293a3cf6f730b5b0eb91
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2023-12-11 14:08:42 +0700

    feat: add group migration,model,controller

---
+++ database/migrations/2023_12_11_065520_create_groups_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('doctor_id')
+                ->constrained('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->string('name');
+            $table->string('description');
+            $table->string('image_url');
+            $table->decimal('price',10,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('groups');
+    }
+};
