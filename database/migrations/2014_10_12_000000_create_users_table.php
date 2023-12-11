4711ba7d3a790a516abfff4015bc9c6f1bb76375
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2023-12-11 11:01:46 +0700

    feat: add Gamer Migration,Model,Controller

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,16 +12,24 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number');
+            $table->string('image_url');
+            $table->date('dob');
             $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
 
+    // $table->uuid('id')->primary();
+    //         $table->foreignUuid('user_id')
+    //             ->constrained('users')
+    //             ->onDelete('cascade')
+    //             ->onUpdate('cascade');
     /**
      * Reverse the migrations.
      */
