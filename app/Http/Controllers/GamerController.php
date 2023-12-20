3702a9851bbe859c1b7dee7fdc66a030c479e5cf
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2023-12-20 13:22:15 +0700

    feat: add gamer register EP; add email validator regis doctor

--- app/Http/Controllers/GamerController.php
+++ app/Http/Controllers/GamerController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Gamer;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class GamerController extends Controller
 {
@@ -18,9 +20,33 @@ class GamerController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'password' => 'required|min:8',
+            'email' => 'required|unique:users|email',
+            'phone_number' => 'required|starts_with:628|min:10|max:16',
+            'dob' => 'required|date',
+        ]);
+        
+        if ($validator->fails()) {
+            return response()->json($validator->errors(),422);
+        }
+
+
+        $user = User::create([
+            'name' => $request['username'],
+            'email' => $request['email'],
+            'password' => $request['password'],
+            'phone_number' => $request['phone_number'],
+            'dob' => $request['dob'],
+            'role' => 'gamer'
+        ]);
+
+        $user->gamer()->create([]);
+
+        return response()->noContent(201);
     }
 
     /**
