e4d4a95260c03cddc57c5c693416a9457d675663
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-01-07 15:58:36 +0700

    refractor: change return response to no content 404

--- app/Http/Controllers/GamerController.php
+++ app/Http/Controllers/GamerController.php
@@ -95,14 +95,10 @@ class GamerController extends Controller
 
         $gamer = Gamer::find($gamerId);
 
-        // if (!$gamer) {
-        //     return response()->json(['message' => 'Gamer not found'], 404);
-        // }
-
         $healthReport = $gamer->healthReport;
 
         if (!$healthReport) {
-            return response()->json(['message' => 'Health report not found'], 401);
+            return response()->json()->setStatusCode(404);
         }
         return response()->json([
             'fisik' => $healthReport->fisik,
